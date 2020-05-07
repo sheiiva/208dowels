fb9d970d4c5e88ac3a8ac164858a52d209eaf717
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-05-07 21:40:03 +0200

    [ADD] init repo

---
+++ dowels.py
@@ -0,0 +1,27 @@
+############################################
+#                MATHEMATICS               #
+############################################
+#                                          #
+#  MONFA-MATAS Patricica & ROZET Corentin  #
+#                                          #
+#          Project : 208dowels_2019        #
+#                                          #
+############################################
+
+
+class Dowels():
+
+    """
+    Main class that allows computation and output printing.
+    """
+
+    def __init__(self):
+        self._d = 0
+
+    def run(self):
+
+        """
+        Run computations and process output printing.
+        """
+
+        return 0
---
+++ usage.py
@@ -0,0 +1,21 @@
+############################################
+#                MATHEMATICS               #
+############################################
+#                                          #
+#  MONFA-MATAS Patricica & ROZET Corentin  #
+#                                          #
+#          Project : 208dowels_2019        #
+#                                          #
+############################################
+
+
+def help() -> None:
+
+    """
+    Show usage of the program.
+    """
+
+    print("USAGE\n"
+        "\t./207demography code [...]\n\n"
+        "DESCRIPTION\n"
+        "\tcode\tcountry code".expandtabs(4))
