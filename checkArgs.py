be3528bd8bafad2e654b8f99a696a8d07bae8ddf
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-05-07 21:59:20 +0200

    [ADD] arguments checking

--- checkArgs.py
+++ checkArgs.py
@@ -17,6 +17,30 @@ class ArgumentManager():
         Check for arguments validity.
         """
 
+        def isInt(value) -> bool:
+            
+            """Expect for value to be an integer."""
+
+            try:
+                int(value)
+            except ValueError:
+                return False
+            else:
+                return True
+
+        aSum = 0
+
+        if len(argv) is not 10:
+            print("Wrong number of arguments. Please retry with -h.")
+            return 84
+        for i in range(1, len(argv)):
+            if not isInt(argv[i]) or int(argv[i]) < 0:
+                print("Arguments must be positive integers. Please retry with -h.")
+                return 84
+            aSum += int(argv[i])
+        if aSum is not 100:
+            print("Arguments' sum must be equal to 100. Please retry with -h.")
+            return 84
         return 0
 
     def needHelp(self, argv) -> bool:
