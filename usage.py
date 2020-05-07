dd4257edad48a20c0f7937511b25d27abaafe50f
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-05-07 21:49:48 +0200

    [ADD] usage

--- usage.py
+++ usage.py
@@ -16,6 +16,7 @@ def help() -> None:
     """
 
     print("USAGE\n"
-        "\t./207demography code [...]\n\n"
-        "DESCRIPTION\n"
-        "\tcode\tcountry code".expandtabs(4))
+            "\t./208dowels O0 O1 O2 O3 O4 O5 O6 O7 O8\n"
+            "\n"
+            "DESCRIPTION\n"
+            "\tOi\tsize of the observed class".expandtabs(4))
