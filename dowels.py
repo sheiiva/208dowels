6f3ff34a7280961392958a384dec85bba9aa7189
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-05-10 19:01:51 +0200

    [ADD] parse Input

--- dowels.py
+++ dowels.py
@@ -9,6 +9,8 @@
 ############################################
 
 
+from sys import argv
+
 from compute import Compute
 
 class Dowels():
@@ -19,12 +21,33 @@ class Dowels():
 
     def __init__(self):
         self._compute = Compute()
+        self._input = []
         self._p = 0
+
+    def parseInput(self):
+        
+        """
+        Parse input into the _input list.
+        """
+
+        for index in range(1, len(argv)):
+            self._input.append(int(argv[index]))
+
+    def statisticalArray(self):
+
+        """
+        Compute and print an array showing observed
+            and theoretical sizes for each statistical class (with totals).
+        """
+
+        return 0
 
     def run(self):
 
         """
         Run computations and process output printing.
         """
+
+        self.parseInput()
 
         return 0
