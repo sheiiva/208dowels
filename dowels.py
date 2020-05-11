af169fb3604a7ac8005a23e1e52ace7ab4e3519f
Patricia <patricia.monfa-matas@epitech.eu>
2020-05-11 18:42:50 +0200

    [ADD] Fit validity

--- dowels.py
+++ dowels.py
@@ -14,6 +14,17 @@ from copy import deepcopy
 
 from compute import Compute
 
+dist_table = [[99, 90, 80, 70, 60, 50, 40, 30, 20, 10, 5, 2, 1],
+              [0.00, 0.02, 0.06, 0.15, 0.27, 0.45, 0.71, 1.07, 1.64, 2.71, 3.84, 5.41, 6.63],
+              [0.02, 0.21, 0.45, 0.71, 1.02, 1.39, 1.83, 2.41, 3.22, 4.61, 5.99, 7.82, 9.21],
+              [0.11, 0.58, 1.01, 1.42, 1.87, 2.37, 2.95, 3.66, 4.64, 6.25, 7.81, 9.84, 11.34],
+              [0.30, 1.06, 1.65, 2.19, 2.75, 3.36, 4.04, 4.88, 5.99, 7.78, 9.49, 11.67, 13.28],
+              [0.55, 1.61, 2.34, 3.00, 3.66, 4.35, 5.13, 6.06, 7.29, 9.24, 11.07, 13.39, 15.09],
+              [0.87, 2.20, 3.07, 3.83, 4.57, 5.35, 6.21, 7.23, 8.56, 10.64, 12.59, 15.03, 16.81],
+              [1.24, 2.83, 3.82, 4.67, 5.49, 6.35, 7.28, 8.38, 9.80, 12.02, 14.07, 16.62, 18.48],
+              [1.65, 3.49, 4.59, 5.53, 6.42, 7.34, 8.35, 9.52, 11.03, 13.36, 15.51, 18.17, 20.09],
+              [2.09, 4.17, 5.38, 6.39, 7.36, 8.34, 9.41, 10.66, 12.24, 14.68, 16.92, 19.68, 21.67],
+              [2.56, 4.87, 6.18, 7.27, 8.30, 9.34, 10.47, 11.78, 13.44, 15.99, 18.31, 21.16, 23.21]]
 
 class Dowels():
 
@@ -28,6 +39,8 @@ class Dowels():
         self._Ox = []
         self._Tx = []
         self._p = 0
+        self._chiSquare = 0
+        self._freedom = 0
 
     def parseInput(self):
 
@@ -129,12 +142,10 @@ class Dowels():
         Compute and print chi-squared.
         """
 
-        chiSquare = 0
-
         for i in range(len(self._Ox)):
-            chiSquare += ((self._Ox[i] - self._Tx[i]) ** 2) / self._Tx[i]
+            self._chiSquare += ((self._Ox[i] - self._Tx[i]) ** 2) / self._Tx[i]
 
-        print("Chi-squared:\t\t{:.3f}".format(chiSquare))
+        print("Chi-squared:\t\t{:.3f}".format(self._chiSquare))
 
     def freedomDegrees(self):
 
@@ -142,9 +153,9 @@ class Dowels():
         Compute and print degrees of freedom.
         """
 
-        freedom = len(self._x) - 2
+        self._freedom = len(self._x) - 2
 
-        print("Degrees of freedom:\t{:d}".format(freedom))
+        print("Degrees of freedom:\t{:d}".format(self._freedom))
 
     def fitValidity(self):
 
@@ -152,7 +163,17 @@ class Dowels():
         Compute and print fit validity.
         """
 
-        print("Distribution:\t\t[..]")
+        v = []
+        
+        for x in range(len(dist_table[self._freedom])):
+                if (dist_table[self._freedom][x] < self._chiSquare) and (x < 12):
+                    v.append(dist_table[0][x + 1])
+        if (len(v) == 2):
+            print("Fit validity::\t{}% < P < {}%".format(v[1], v[0]))
+        elif (len(v) == 0):
+            print("Fit validity::\tP > 99%")
+        else:
+            print("Fit validity::\tP < 1%")
 
     def run(self):
 
